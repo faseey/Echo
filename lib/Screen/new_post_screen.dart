7a98bbcd7881f872beebf47cd8883a28b184c860
Awais Hassan <muhammadawaishassan1@gmail.com>
2025-06-10 17:05:48 +0500

    profile

--- lib/Screen/new_post_screen.dart
+++ lib/Screen/new_post_screen.dart
@@ -1,10 +1,15 @@
-import 'dart:convert';
+
+
+
+/*import 'dart:convert';
 import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../component/MyDrawer.dart';
+import '../constant/post_card_builder.dart';
 import '../controllers/new_post_cont.dart';
 
 class NewPostScreen extends StatelessWidget {
@@ -33,174 +38,199 @@ class NewPostScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     controller.loadImagePostsFromFirestore();
 
-    return GetBuilder<NewPostController>(
-      builder: (_) {
-        final posts = controller.firestorePosts;
+    return GetBuilder<NewPostController>(builder: (_) {
+      final posts = controller.firestorePosts;
 
-        return Scaffold(
-
-          appBar: AppBar(
-            backgroundColor: Colors.grey.shade200,
-            iconTheme: IconThemeData(color: Colors.white),
-            title: Text(
-              "EchoGram",
-              style: TextStyle(
-                fontFamily: 'Billabong',
-                fontSize: 30,
-                color: Colors.pinkAccent,
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade200,
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            "Echo",
+            style: TextStyle(
+              fontFamily: 'Billabong',
+              fontSize: 20,
+              color: Colors.blueGrey,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        drawer: MyDrawer(),
+        backgroundColor: Colors.grey[100],
+        body: Column(
+          children: [
+            Expanded(
+              child: posts.isEmpty
+                  ? Center(child: Text("No posts yet", style: TextStyle(color: Colors.black38)))
+                  : ListView.builder(
+                padding: EdgeInsets.all(10),
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  return PostCardBuilder(
+                    post: posts[index],
+                    index: index,
+                    onDelete: () {
+                      controller.deletePostAtIndex(index);
+                    },
+                    onViewImage: () {
+                      _showFullImage(context, posts[index].imageBase64);
+                    },
+                  );
+                },
               ),
             ),
-            centerTitle: true,
-          ),
-          drawer: MyDrawer(),
-          body: Column(
-            children: [
-              Expanded(
-                child: posts.isEmpty
-                    ? Center(child: Text("No posts yet", style: TextStyle(color: Colors.black38)))
-                    : ListView.builder(
-                  padding: EdgeInsets.all(10),
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    final post = posts[index];
-                    Uint8List? imageBytes;
-
-                    try {
-                      if (post.imageBase64.isNotEmpty) {
-                        imageBytes = base64Decode(post.imageBase64);
-                      }
-                    } catch (e) {
-                      print("Error decoding image: $e");
-                    }
-
-                    return Card(
-                      color: Colors.grey[900],
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                      margin: EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              if (imageBytes != null) {
-                                _showFullImage(context, post.imageBase64);
-                              }
-                            },
-                            onLongPress: () {
-                              Get.defaultDialog(
-                                title: "Delete Post",
-                                middleText: "Are you sure you want to delete this post?",
-                                textCancel: "No",
-                                textConfirm: "Yes",
-                                confirmTextColor: Colors.white,
-                                onConfirm: () {
-                                  controller.deletePostAtIndex(index);
-                                  Get.back();
-                                },
-                              );
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-                              child: imageBytes != null
-                                  ? AspectRatio(
-                                aspectRatio: 1, // Square like Instagram
-                                child: Image.memory(
-                                  imageBytes,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                ),
-                              )
-                                  : Container(
-                                height: 300,
-                                color: Colors.grey[700],
-                                child: Icon(Icons.broken_image, color: Colors.white, size: 50),
-                              ),
-
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                            child: Text(
-                              post.content,
-                              style: TextStyle(color: Colors.white, fontSize: 14),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 12),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.favorite_border, color: Colors.pinkAccent),
-                                    SizedBox(width: 6),
-                                    Text("Like", style: TextStyle(color: Colors.white)),
-                                  ],
-                                ),
-                                Text(
-                                  post.date.split('T').first,
-                                  style: TextStyle(color: Colors.grey, fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              // Post Input
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                child: TextField(
-                  controller: controller.contentController,
-                  maxLines: 2,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[800],
-                    hintText: "Write a caption...",
-                    hintStyle: TextStyle(color: Colors.grey[400]),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: TextField(
+                controller: controller.contentController,
+                maxLines: 2,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[800],
+                  hintText: "Write a caption...",
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
                   ),
                 ),
               ),
-              // Buttons
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton.icon(
-                      onPressed: () => controller.pickImage(ImageSource.camera),
-                      icon: Icon(Icons.camera_alt),
-                      label: Text("Camera"),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.pinkAccent,
-                        foregroundColor: Colors.white,
-                      ),
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () => controller.pickImage(ImageSource.gallery),
-                      icon: Icon(Icons.photo),
-                      label: Text("Gallery"),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.deepPurple,
-                        foregroundColor: Colors.white,
-                      ),
-                    ),
-                  ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () => controller.pickImage(ImageSource.camera),
+                  icon: Icon(Icons.camera_alt),
+                  label: Text("Camera"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                  ),
                 ),
+                ElevatedButton.icon(
+                  onPressed: () => controller.pickImage(ImageSource.gallery),
+                  icon: Icon(Icons.photo),
+                  label: Text("Gallery"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}*/
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../component/MyDrawer.dart';
+import '../constant/post_card_builder.dart';
+import '../controllers/new_post_cont.dart';
+
+
+class NewPostScreen extends StatelessWidget {
+  NewPostScreen({Key? key}) : super(key: key);
+  final NewPostController controller = Get.put(NewPostController());
+
+  @override
+  Widget build(BuildContext context) {
+    controller.loadImagePostsFromFirestore();
+
+    return GetBuilder<NewPostController>(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(color: Colors.black),
+          title: Text(
+            "Echo",
+            style: TextStyle(
+              fontFamily: 'Billabong',
+              fontSize: 30,
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        drawer:  MyDrawer(),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Expanded(
+              child: controller.firestorePosts.isEmpty
+                  ? const Center(
+                  child: Text("No posts yet",
+                      style: TextStyle(color: Colors.black38)))
+                  : ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: controller.firestorePosts.length,
+                itemBuilder: (context, index) {
+                  return PostCardBuilder(
+                    post: controller.firestorePosts[index],
+                    index: index,
+                    onDelete: () => controller.deletePostAtIndex(index),
+                  );
+                },
+              ),
+            ),
+            _buildCreatePostSection(),
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget _buildCreatePostSection() {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          const Divider(height: 1),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                backgroundImage: AssetImage('assets/user_placeholder.png'),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: TextField(
+                  controller: controller.contentController,
+                  decoration: const InputDecoration(
+                    hintText: "Write a caption...",
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(vertical: 12),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () => controller.pickImage(ImageSource.camera),
+                    icon: const Icon(Icons.camera_alt, color: Colors.black),
+                  ),
+                  IconButton(
+                    onPressed: () => controller.pickImage(ImageSource.gallery),
+                    icon: const Icon(Icons.photo_library, color: Colors.black),
+                  ),
+                ],
               ),
             ],
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
---
+++ lib/constant/post_card_builder.dart
@@ -0,0 +1,266 @@
+/*import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/post_model.dart'; // Update with actual model import
+
+class PostCardBuilder extends StatelessWidget {
+  final dynamic post;
+  final int index;
+  final VoidCallback onDelete;
+  final VoidCallback onViewImage;
+
+  const PostCardBuilder({
+    Key? key,
+    required this.post,
+    required this.index,
+    required this.onDelete,
+    required this.onViewImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Uint8List? imageBytes;
+    try {
+      if (post.imageBase64.isNotEmpty) {
+        imageBytes = base64Decode(post.imageBase64);
+      }
+    } catch (e) {
+      print("Error decoding image: $e");
+    }
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header with user info
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 18,
+                  backgroundImage: AssetImage('assets/user_placeholder.png'),
+                ),
+                SizedBox(width: 12),
+                Text(
+                  post.username ?? "User",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {
+                    Get.defaultDialog(
+                      title: "Delete Post",
+                      middleText: "Are you sure you want to delete this post?",
+                      textCancel: "No",
+                      textConfirm: "Yes",
+                      confirmTextColor: Colors.white,
+                      onConfirm: () {
+                        onDelete();
+                        Get.back();
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+
+
+
+
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PostCardBuilder extends StatelessWidget {
+  final dynamic post;
+  final int index;
+  final VoidCallback onDelete;
+
+  const PostCardBuilder({
+    Key? key,
+    required this.post,
+    required this.index,
+    required this.onDelete,
+  }) : super(key: key);
+
+  void _showFullImage(BuildContext context, String base64Image) {
+    try {
+      final imageBytes = base64Decode(base64Image);
+      showDialog(
+        context: context,
+        builder: (_) => Dialog(
+          backgroundColor: Colors.black,
+          child: InteractiveViewer(
+            child: Image.memory(imageBytes),
+          ),
+        ),
+      );
+    } catch (e) {
+      Get.snackbar("Error", "Failed to load full image.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Uint8List? imageBytes;
+    try {
+      if (post.imageBase64.isNotEmpty) {
+        imageBytes = base64Decode(post.imageBase64);
+      }
+    } catch (e) {
+      print("Error decoding image: $e");
+    }
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header with user info
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  backgroundImage: AssetImage('assets/user_placeholder.png'),
+                ),
+                const SizedBox(width: 12),
+                Text(
+                  post.username ?? "User",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () => _showDeleteDialog(context),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+
+          // Post image
+          GestureDetector(
+            onTap: () => _showFullImage(context, post.imageBase64),
+            child: imageBytes != null
+                ? Image.memory(
+              imageBytes,
+              width: double.infinity,
+              height: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            )
+                : Container(
+              height: MediaQuery.of(context).size.width,
+              color: Colors.grey[200],
+              child: const Center(child: Icon(Icons.broken_image, size: 50)),
+            ),
+          ),
+          Divider(),
+
+          // Action buttons
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.favorite_border, size: 28),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.chat_bubble_outline, size: 28),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send, size: 28),
+                  onPressed: () {},
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.bookmark_border, size: 28),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+
+          // Likes count
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "62 likes",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 4),
+
+          // Caption
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: RichText(
+              text: TextSpan(
+                style: const TextStyle(color: Colors.black),
+                children: [
+                  TextSpan(
+                    text: "${post.username} ",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(text: post.content),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 4),
+
+          // Comments
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "View all 21 comments",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          const SizedBox(height: 4),
+
+          // Timestamp
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              post.date.split('T').first,
+              style: const TextStyle(color: Colors.grey, fontSize: 12),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showDeleteDialog(BuildContext context) {
+    Get.defaultDialog(
+      title: "Delete Post",
+      middleText: "Are you sure you want to delete this post?",
+      textCancel: "No",
+      textConfirm: "Yes",
+      confirmTextColor: Colors.white,
+      onConfirm: () {
+        onDelete();
+        Get.back();
+      },
+    );
+  }
+}
+
+
